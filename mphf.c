18e6337ad7085cd70d999088a7688b8e6d8360c7
cosmomapanje <cosmo.mapanje@gmail.com>
2016-01-19 18:27:46 +0800

    first commit

---
+++ mphf.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define MAX_CHAR_NUM 26
+char freq_of_char[MAX_CHAR_NUM] = {0};
+
+static enum {LEFT, RIGHT};
+static char *static_site[] = {"do", "end", "else", "case", "downto", "goto",
+			      "to", "otherwise", "type", "while", "const", "div",
+			      "and", "set", "or", "of", "mod", "file", "record",
+			      "packed", "not", "then", "procedure", "with", "repeat",
+			      "var", "in", "array", "if", "nil", "for",
+			      "begin", "until", "label", "function", "program"};
+
+struct freq_tree {
+	struct freq_tree *left;
+	struct freq_tree *right;
+	unsigned char freq;
+	unsigned char str_len;
+        char *str;
+};
+
+struct freq_tree *tree_root = NULL;
+
+struct freq_tree *insert_into_tree(char *instring)
+{
+	unsigned char this_freq = 0;
+	struct freq_tree *tmp = NULL;
+	struct freq_tree *seek = tree_root;
+	struct freq_tree *papa = NULL;
+	char left_or_right = LEFT;
+
+	assert(instring != NULL);
+	tmp = (struct freq_tree *)malloc(sizeof(struct freq_tree));
+	if (tmp == NULL) {
+		printf("malloc error!\n");
+		exit(-1);
+	}
+	tmp->left = NULL;
+	tmp->right = NULL;
+	tmp->str = instring;
+	tmp->str_len = strlen(instring);
+	tmp->freq = this_freq = freq_of_char[*instring - 'a'] + freq_of_char[*(instring + tmp->str_len - 1) - 'a'] + tmp->str_len;
+
+	/* the first noot, which is root */
+	if (seek == NULL) {
+		tree_root = tmp;
+		return tree_root;
+	}
+	
+	while (seek != NULL) {
+		papa = seek;
+		if (seek->freq > tmp->freq) {
+			seek = seek->left;
+			left_or_right = LEFT;
+		} else if (seek->freq < tmp->freq){
+			seek = seek->right;
+			left_or_right = RIGHT;
+		} else {
+			seek = seek->right;
+			left_or_right = RIGHT;
+			break;
+		}
+	}
+
+	switch (left_or_right) {
+	case LEFT:
+		papa->left = tmp;
+		break;
+	case RIGHT:
+		if (seek == NULL) {
+			papa->right = tmp;
+		} else {
+			tmp->right = seek;
+			papa->right = tmp;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+
+int calc_string(char *one_str)
+{
+	int num = strlen(one_str);
+	int key = num + (*one_str - 'a') + (*(one_str + num - 1) - 'a');
+	printf("str(%s) \t= len(%d), key(%d, %d)\n", one_str, num, (*one_str - 'a'), (*(one_str + num - 1) - 'a'));
+	freq_of_char[*one_str - 'a']++;
+	freq_of_char[*(one_str + num - 1) - 'a']++;
+}
+
+void print_freq()
+{
+	int i = 0;
+	char *tmp = NULL;
+	for (; i < MAX_CHAR_NUM; i++) {
+		printf("%c shows %d times!\n", i + 'a', freq_of_char[i]);
+	}
+
+	for (i = 0; i < sizeof(static_site)/sizeof(char *); i++) {
+		tmp = *(static_site + i);
+		printf("%s has %d freq!\n", static_site[i], freq_of_char[*tmp - 'a'] + freq_of_char[*(tmp + strlen(tmp) - 1) - 'a']);
+	}
+}
+
+void print_freq_by_tree(struct freq_tree *fnode)
+{
+	if (fnode == NULL)
+		return;
+
+	print_freq_by_tree(fnode->left);
+	printf("%s = %d\n", fnode->str, fnode->freq);
+	print_freq_by_tree(fnode->right);
+	
+}
+
+void order_freq()
+{
+	int i = 0;
+	for (;i < sizeof(static_site)/sizeof(char *); i++) {
+		insert_into_tree(static_site[i]);
+	}
+}
+
+int hash_func(char *str)
+{
+	/* hash value = key length + first character + last charater */
+	unsigned char key = 0;
+	key = strlen(str);
+	key += (*str + *(str + key - 1));
+	return key;
+}
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (i < sizeof(static_site)/sizeof(char *)) {
+//		printf("%d : %s : ", i, static_site[i]);
+//		printf("key = %d\n", hash_func(static_site[i]));
+		calc_string(static_site[i++]);
+	}
+	print_freq();
+	order_freq();
+	print_freq_by_tree(tree_root);
+	return 0;
+}
