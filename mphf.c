96eab89a55ba6eb4760a4847f195f0b24914cbb4
cosmomapanje <cosmo.mapanje@gmail.com>
2016-01-21 18:23:06 +0800

    fix qsort issue, and sort word by freq of single char, no backtracking now

--- mphf.c
+++ mphf.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 #define MAX_CHAR_NUM 26
+#define MAX_KEY_WORD 36
 char freq_of_char[MAX_CHAR_NUM] = {0};
 
-static enum {LEFT, RIGHT};
 static char *static_site[] = {"do", "end", "else", "case", "downto", "goto",
 			      "to", "otherwise", "type", "while", "const", "div",
 			      "and", "set", "or", "of", "mod", "file", "record",
@@ -14,114 +14,45 @@ static char *static_site[] = {"do", "end", "else", "case", "downto", "goto",
 			      "var", "in", "array", "if", "nil", "for",
 			      "begin", "until", "label", "function", "program"};
 
-struct freq_tree {
-	struct freq_tree *left;
-	struct freq_tree *right;
-	unsigned char freq;
-	unsigned char str_len;
-        char *str;
-};
+char *static_site_in_order[MAX_KEY_WORD] = {NULL}; 
+
+struct static_sites {
+	char char1;
+	char char2;
+	char *str;
+	char strlengh;
+	char key_value;
+} ss[MAX_KEY_WORD] = {{'d','o',"do", 2}, {'e','d',"end", 3}, {'e','e',"else", 4},
+		      {'c','e',"case", 4}, {'d','o',"downto", 6}, {'g','o',"goto", 4},
+		      {'t','o',"to", 2}, {'o','e',"otherwise", 9}, {'t','e',"type", 4},
+		      {'w','e',"while", 5}, {'c','t',"const", 5}, {'d','v',"div", 3},
+		      {'a','d',"and", 3}, {'s','t',"set", 3}, {'o','r',"or", 2},
+		      {'o','f',"of", 2}, {'m','d',"mod", 3}, {'f','e',"file", 4},
+		      {'r','d',"record", 6}, {'p','d',"packed", 6}, {'n','t',"not", 3},
+		      {'t','n',"then", 4}, {'p','e',"procedure", 9}, {'w','h',"with", 4},
+		      {'r','t',"repeat", 6}, {'v','r',"var", 3}, {'i','n',"in", 2},
+		      {'a','y',"array", 5}, {'i','f',"if", 2}, {'n','l',"nil", 3},
+		      {'f','o',"for", 3}, {'b','n',"begin", 5}, {'u','l',"until", 5},
+		      {'l','l',"label", 5}, {'f','n',"function", 8}, {'p','m',"program", 7}};
+
+struct static_sites *ssp[MAX_KEY_WORD] = {NULL};
 
 struct freq_char {
 	unsigned char c;
 	unsigned char freq;
+	unsigned char key;
 };
 
 struct freq_char *freq_of_char_in_order[MAX_CHAR_NUM] = {NULL};
-
-struct freq_tree *tree_root = NULL;
-
-struct freq_tree *insert_into_tree(char *instring)
-{
-	unsigned char this_freq = 0;
-	struct freq_tree *tmp = NULL;
-	struct freq_tree *seek = tree_root;
-	struct freq_tree *papa = NULL;
-	char left_or_right = LEFT;
-
-	assert(instring != NULL);
-	tmp = (struct freq_tree *)malloc(sizeof(struct freq_tree));
-	if (tmp == NULL) {
-		printf("malloc error!\n");
-		exit(-1);
-	}
-	tmp->left = NULL;
-	tmp->right = NULL;
-	tmp->str = instring;
-	tmp->str_len = strlen(instring);
-	tmp->freq = this_freq = freq_of_char[*instring - 'a'] + freq_of_char[*(instring + tmp->str_len - 1) - 'a'] + tmp->str_len;
-
-	/* the first noot, which is root */
-	if (seek == NULL) {
-		tree_root = tmp;
-		return tree_root;
-	}
-	
-	while (seek != NULL) {
-		papa = seek;
-		if (seek->freq > tmp->freq) {
-			seek = seek->left;
-			left_or_right = LEFT;
-		} else if (seek->freq < tmp->freq){
-			seek = seek->right;
-			left_or_right = RIGHT;
-		} else {
-			seek = seek->right;
-			left_or_right = RIGHT;
-			break;
-		}
-	}
-
-	switch (left_or_right) {
-	case LEFT:
-		papa->left = tmp;
-		break;
-	case RIGHT:
-		if (seek == NULL) {
-			papa->right = tmp;
-		} else {
-			tmp->right = seek;
-			papa->right = tmp;
-		}
-		break;
-	default:
-		break;
-	}
-}
-
+struct freq_char *freq_of_char_in_alph[MAX_CHAR_NUM] = {NULL};
 
 void calc_string(char *one_str)
 {
 	int num = strlen(one_str);
 	int key = num + (*one_str - 'a') + (*(one_str + num - 1) - 'a');
-	printf("str(%s) \t= len(%d), key(%d, %d)\n", one_str, num, (*one_str - 'a'), (*(one_str + num - 1) - 'a'));
+//	printf("str(%s) \t= len(%d), key(%d, %d)\n", one_str, num, (*one_str - 'a'), (*(one_str + num - 1) - 'a'));
 	freq_of_char[*one_str - 'a']++;
 	freq_of_char[*(one_str + num - 1) - 'a']++;
-}
-
-void print_freq()
-{
-	int i = 0;
-	char *tmp = NULL;
-	for (; i < MAX_CHAR_NUM; i++) {
-		printf("%c shows %d times!\n", i + 'a', freq_of_char[i]);
-	}
-
-	for (i = 0; i < sizeof(static_site)/sizeof(char *); i++) {
-		tmp = *(static_site + i);
-		printf("%s has %d freq!\n", static_site[i], freq_of_char[*tmp - 'a'] + freq_of_char[*(tmp + strlen(tmp) - 1) - 'a']);
-	}
-}
-
-void print_freq_by_tree(struct freq_tree *fnode)
-{
-	if (fnode == NULL)
-		return;
-
-	print_freq_by_tree(fnode->left);
-	printf("%s = %d\n", fnode->str, fnode->freq);
-	print_freq_by_tree(fnode->right);
-	
 }
 
 void swap(struct freq_char **first, struct freq_char **second)
@@ -141,24 +72,15 @@ void qsort_freq(int mid_pos, int num)
 		return;
 	}
 
-	for (i = mid_pos + 1; i < num; i++) {
-		if (freq_of_char_in_order[i]->freq < freq_of_char_in_order[mid_pos]->freq) {
+	for (i = mid_pos + 1; i <= num; i++) {
+		if (freq_of_char_in_order[i]->freq >= freq_of_char_in_order[mid_pos]->freq) {
 			swap(&freq_of_char_in_order[++m], &freq_of_char_in_order[i]);
 		}
 	}
 	
-	for (i = 0; i < MAX_CHAR_NUM; i++) {
-		printf("%d ", freq_of_char_in_order[i]->freq);
-	}
-	printf("\n");
-	printf("mid_pos = %d, m = %d\n", mid_pos, m);
+	//printf("%*s|%*s|\n", (mid_pos<<1), "", (m<<1) - (mid_pos<<1) - 1, ""); 
 	swap(&freq_of_char_in_order[mid_pos], &freq_of_char_in_order[m]);
 
-	for (i = 0; i < MAX_CHAR_NUM; i++) {
-		printf("%d ", freq_of_char_in_order[i]->freq);
-	}
-	printf("\n");
-	printf("%d, %d\n", mid_pos, m - 1);
 	qsort_freq(mid_pos, m - 1);
 	qsort_freq(m + 1, num);
 }
@@ -176,25 +98,60 @@ void order_freq()
 		}
 		fc->freq = freq_of_char[i];
 		fc->c = i + 'a';
+		fc->key = 0;
 		freq_of_char_in_order[i] = fc;
+		freq_of_char_in_alph[i] = fc;
 	}
 
-
-	/* just for test */
-	for (i = 0; i < MAX_CHAR_NUM; i++) {
-		fc = freq_of_char_in_order[i];
-		printf("---->%c = %d\n", fc->c, fc->freq);
-	}
-	qsort_freq(0, MAX_CHAR_NUM);
-
-	for (i = 0; i < MAX_CHAR_NUM; i++) {
-		fc = freq_of_char_in_order[i];
-		printf("----> new %c = %d\n", fc->c, fc->freq);
-	}
-	
+	qsort_freq(0, MAX_CHAR_NUM - 1);
+#if 0
+	/* we could delete these code, we don't use it anymore */
 	/* ordering string by char's freq. */
 	for (i = 0;i < sizeof(static_site)/sizeof(char *); i++) {
 		insert_into_tree(static_site[i]);
+	}
+#endif
+}
+void order_freq_of_word()
+{
+	int i = 0;
+	int j = 0;
+	int s = 0;
+	int num = 0;
+	for (; i < MAX_CHAR_NUM; i++) {
+		for (j = 0; j <= i; j++) {
+//			printf("==== > %c %c\n",freq_of_char_in_order[i]->c, freq_of_char_in_order[j]->c);
+			for (s = 0; s < MAX_KEY_WORD; s++) {
+				if ((ss[s].char1 == freq_of_char_in_order[i]->c) && (ss[s].char2 == freq_of_char_in_order[j]->c)) {
+					printf("num = %d, %s\n", num, ss[s].str);
+					static_site_in_order[num] = static_site[s];
+					ssp[num] = &ss[s];
+					num++;
+					continue;
+				} else if ((ss[s].char2 == freq_of_char_in_order[i]->c) && (ss[s].char1 == freq_of_char_in_order[j]->c)) {
+					printf("num = %d, %s\n", num, ss[s].str);
+					static_site_in_order[num] = static_site[s];
+					ssp[num] = &ss[s];
+					num++;
+					continue;
+				} else {
+					continue;
+				}
+			}
+		}
+	}
+
+	for (i = 0; i < MAX_KEY_WORD; i++)
+		printf("%d = %s\n",i, static_site_in_order[i]);
+	
+}
+
+void processing_char()
+{
+	int i = 0;
+	for (; i < MAX_KEY_WORD; i++) {
+		ssp[i]->key_value = freq_of_char_in_alph[ssp[i]->char1 - 'a']->key + freq_of_char_in_alph[ssp[i]->char2 - 'a']->key + ssp[i]->strlengh;
+		printf("[%s = %c ... %d ... %c] = %d\n", ssp[i]->str, ssp[i]->char1, ssp[i]->strlengh, ssp[i]->char2, ssp[i]->key_value);
 	}
 }
 
@@ -210,13 +167,17 @@ int hash_func(char *str)
 int main(int argc, char *argv[])
 {
 	int i = 0;
+	
 	while (i < sizeof(static_site)/sizeof(char *)) {
-//		printf("%d : %s : ", i, static_site[i]);
-//		printf("key = %d\n", hash_func(static_site[i]));
 		calc_string(static_site[i++]);
 	}
-	print_freq();
+	
 	order_freq();
-	print_freq_by_tree(tree_root);
+	for (i = 0; i < MAX_CHAR_NUM; i++) {
+		printf("%c = %d\n", freq_of_char_in_order[i]->c, freq_of_char_in_order[i]->freq);
+	}
+	order_freq_of_word();
+	processing_char();
+//	print_freq_by_tree(tree_root);
 	return 0;
 }
